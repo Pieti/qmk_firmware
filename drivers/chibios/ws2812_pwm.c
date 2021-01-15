29973663ff8737c23d6f8a98a01b918145ef5cdf
kb-elmo <lorwel@mailbox.org>
2021-01-15 22:42:30 +0100

    Add BGR byte order for WS2812 drivers (#11562)
    * add byte order bgr for ws2812
    
    * update docs for driver change
    
    * Update ws2812_driver.md
    
    * Update docs/ws2812_driver.md
    
    Co-authored-by: Ryan <fauxpark@gmail.com>
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- drivers/chibios/ws2812_pwm.c
+++ drivers/chibios/ws2812_pwm.c
@@ -179,7 +179,48 @@
  *
  * @return                          The bit index
  */
+<<<<<<< HEAD
 #   define WS2812_BLUE_BIT(led, bit) WS2812_BIT((led), 2, (bit))
+=======
+#    define WS2812_BLUE_BIT(led, bit) WS2812_BIT((led), 2, (bit))
+
+#elif (WS2812_BYTE_ORDER == WS2812_BYTE_ORDER_BGR)
+/**
+ * @brief   Determine the index in @ref ws2812_frame_buffer "the frame buffer" of a given red bit
+ *
+ * @note    The red byte is the middle byte in the color packet
+ *
+ * @param[in] led:                  The led index [0, @ref RGBLED_NUM)
+ * @param[in] bit:                  The bit number [0, 7]
+ *
+ * @return                          The bit index
+ */
+#    define WS2812_RED_BIT(led, bit) WS2812_BIT((led), 2, (bit))
+
+/**
+ * @brief   Determine the index in @ref ws2812_frame_buffer "the frame buffer" of a given green bit
+ *
+ * @note    The red byte is the first byte in the color packet
+ *
+ * @param[in] led:                  The led index [0, @ref RGBLED_NUM)
+ * @param[in] bit:                  The bit number [0, 7]
+ *
+ * @return                          The bit index
+ */
+#    define WS2812_GREEN_BIT(led, bit) WS2812_BIT((led), 1, (bit))
+
+/**
+ * @brief   Determine the index in @ref ws2812_frame_buffer "the frame buffer" of a given blue bit
+ *
+ * @note    The red byte is the last byte in the color packet
+ *
+ * @param[in] led:                  The led index [0, @ref RGBLED_NUM)
+ * @param[in] bit:                  The bit index [0, 7]
+ *
+ * @return                          The bit index
+ */
+#    define WS2812_BLUE_BIT(led, bit) WS2812_BIT((led), 0, (bit))
+>>>>>>> Add BGR byte order for WS2812 drivers (#11562)
 #endif
 
 /* --- PRIVATE VARIABLES ---------------------------------------------------- */
